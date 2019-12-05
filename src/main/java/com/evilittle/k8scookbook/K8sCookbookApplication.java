1cd250bfa62d493b46a6ab31e1fc076dadc38541
rexlee8776 <rexlee8776@gmail.com>
2019-12-05 16:58:13 +0800

    initial the repo with spring web and jpa
    Signed-off-by: rexlee8776 <rexlee8776@gmail.com>

---
+++ src/main/java/com/evilittle/k8scookbook/K8sCookbookApplication.java
@@ -0,0 +1,13 @@
+package com.evilittle.k8scookbook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class K8sCookbookApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(K8sCookbookApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/evilittle/k8scookbook/K8sCookbookApplicationTests.java
@@ -0,0 +1,13 @@
+package com.evilittle.k8scookbook;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class K8sCookbookApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
